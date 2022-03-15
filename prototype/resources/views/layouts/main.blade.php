0dea6ee5eda7249d199a70a5f53628889b1c1dda
DJ <dilolwajorge.dj@gmail.com>
2022-03-15 11:31:01 +0100

    Add ícone Navbar

--- prototype/resources/views/layouts/main.blade.php
+++ prototype/resources/views/layouts/main.blade.php
@@ -17,8 +17,7 @@
     <!-- Navbar -->
     <nav class="navbar sticky-top navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Sticky top</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand" href="#"><img src="/img/Tchiowacorp.png" alt=""></a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
