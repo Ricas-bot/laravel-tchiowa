c5c861fbeb88c895e0e72024bcc8fca82b8e6038
Kelsio Ricardo <kelsiomiguel2000@hotmail.com>
2022-03-15 19:58:01 +0100

    Add Hero n some update(navbar & footer)

--- prototype/resources/views/layouts/main.blade.php
+++ prototype/resources/views/layouts/main.blade.php
@@ -28,21 +28,13 @@
             <a class="nav-link active scrollto" aria-current="page" href="#home">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link scrollto" href="#about">about</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <a class="nav-link scrollto" href="#about">Sobre</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link scrollto" href="#services">Serviços</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link scrollto" aria-current="page" href="#contact">Contacto</a>
           </li>
         </ul>
       </div>
@@ -60,7 +52,7 @@
     <footer class="py-5">
       <div class="row">
         <div class="col-2">
-          <h5>Section</h5>
+          <h5>Links utéis</h5>
           <ul class="nav flex-column">
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
@@ -71,7 +63,7 @@
         </div>
 
         <div class="col-2">
-          <h5>Section</h5>
+          <h5>Nossos serviços</h5>
           <ul class="nav flex-column">
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
@@ -95,7 +87,7 @@
       </div>
 
       <div class="d-flex justify-content-between py-4 my-4 border-top">
-        <p>&copy; 2021 Company, Inc. All rights reserved.</p>
+        <p>&copy; 2022 Tchiowa, Corp. All rights reserved.</p>
         <ul class="list-unstyled d-flex">
           <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
           <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
--- prototype/resources/views/welcome.blade.php
+++ prototype/resources/views/welcome.blade.php
@@ -3,7 +3,14 @@
 
 <!-- ===== Hero section ===== -->
 <section id="home" class="d-flex align-items-center">
-    
+    <div class="container" data-aos="zoom-in">
+        <div class="row">
+            <div class="col-lg-9 text-center text-lg-start">
+                <h2>Bem-vindo ao site da Tchiowa Corp</h3>
+                <p> Temos diversificados serviços que irão melhorar a sua visibilidade no mercado, aumentar o seu conhecimento e muito mais.</p>
+            </div>
+        </div>
+    </div>
 </section>
   <!-- End of Section -->
 
