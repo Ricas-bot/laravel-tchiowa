1627816349cc2236ec4c1c0c884bc4f816392fd5
DJ <dilolwajorge.dj@gmail.com>
2022-03-16 10:52:03 +0100

    Hero updates(H1, H2)

--- prototype/resources/views/welcome.blade.php
+++ prototype/resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
     <div class="container" data-aos="zoom-in">
         <div class="row">
             <div class="col-lg-9 text-center text-lg-start">
-                <h2>Bem-vindo ao site da Tchiowa Corp</h3>
-                <p> Temos diversificados serviços que irão melhorar a sua visibilidade no mercado, aumentar o seu conhecimento e muito mais.</p>
+                <h1>Bem-vindo ao site da Tchiowa Corp</h1>
+                <h2> Temos diversificados serviços que irão melhorar a sua visibilidade no mercado, aumentar o seu conhecimento e muito mais.</h2>
             </div>
         </div>
     </div>
