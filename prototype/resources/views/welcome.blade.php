1d6d015e2f7b306fecae1076559c41cc531e3fdc
DJ <dilolwajorge.dj@gmail.com>
2022-03-16 11:13:58 +0100

    Add sections

--- prototype/resources/views/welcome.blade.php
+++ prototype/resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
         <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
       </div>
       <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+        <h1 class="display-5 fw-bold lh-1 mb-3">Sobre nós</h1>
         <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
         <div class="d-grid gap-2 d-md-flex justify-content-md-start">
           <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
@@ -36,6 +36,24 @@
 
       </div>
     </section>
+<!-- End of Section -->
+
+<!-- ===== Service section ===== -->
+<section id="service" class="service">
+  <h1>Serviços</h1>
+</section>
+<!-- End of Section -->
+
+<!-- ===== Portfolio section ===== -->
+<section id="portfolio" class="portfolio">
+  <h1>Portfólio</h1>
+</section>
+<!-- End of Section -->
+
+<!-- ===== Contact section ===== -->
+<section id="contact" class="contact">
+  <h1>Contactos</h1>
+</section>
 <!-- End of Section -->
 
 @endsection
